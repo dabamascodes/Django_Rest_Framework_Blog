6ff4faca59eaf1cb4a23b51fee50f4f5f831990b
dabamascodes <daniel.6@live.com.mx>
2025-10-03 12:59:48 -0600

    28. Tabular inline

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -12,6 +12,13 @@ class CategoryAdmin(admin.ModelAdmin):
     ordering = ('name',)
     readonly_fields = ('id',)
     
+ 
+class HeadingInline(admin.TabularInline):
+    model = Heading
+    extra = 1
+    fields = ('title', 'level', 'order', 'slug') 
+    prepopulated_fields = {'slug': ('title',)}
+    ordering = ('order',)
     
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
@@ -29,3 +36,13 @@ class PostAdmin(admin.ModelAdmin):
             'fields': ('status', 'created_at', 'updated_at')
         }),
     )
+    inlines = [HeadingInline]
+    
+    
+# @admin.register(Heading)
+# class HeadingAdmin(admin.ModelAdmin):
+#     list_display = ('title', 'post', 'level', 'order')
+#     search_fields = ('title', 'post__title')
+#     list_filter = ('level', 'post')
+#     ordering = ('post', 'order')
+#     prepopulated_fields = {'slug': ('title',)}
