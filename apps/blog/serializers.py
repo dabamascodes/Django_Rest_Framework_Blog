ab2fda934c7023d011042c3ee8f1c102d023fafa
dabamascodes <daniel.6@live.com.mx>
2025-12-08 12:41:39 -0700

    29. Postman

--- apps/blog/serializers.py
+++ apps/blog/serializers.py
@@ -3,30 +3,20 @@ from rest_framework import serializers
 from .models import Post, Category, Heading
 
 
-class PostSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Post
-        fields = "__all__"
-    
-    
-class PostListSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = [
-            "id",
-            "title",
-            "description",
-            "thumbnail",
-            "slug",
-            "category",
-        ]
-    
-
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = "__all__"
-    
+        
+
+class CategoryListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = [
+            "name",
+            "slug"
+        ]    
+        
     
 class HeadingSerializer(serializers.ModelSerializer):
     class Meta:
@@ -37,3 +27,27 @@ class HeadingSerializer(serializers.ModelSerializer):
             "level",
             "order",
         ]
+        
+            
+class PostSerializer(serializers.ModelSerializer):
+    category = CategoryListSerializer()
+    headings = HeadingSerializer(many=True)
+    class Meta:
+        model = Post
+        fields = "__all__"
+    
+    
+class PostListSerializer(serializers.ModelSerializer):
+    category = CategoryListSerializer()
+    class Meta:
+        model = Post
+        fields = [
+            "id",
+            "title",
+            "description",
+            "thumbnail",
+            "slug",
+            "category",
+        ]
+    
+
